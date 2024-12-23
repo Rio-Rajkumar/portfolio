a6579a3eeb7bddb956ae3ccceab0dcf00e3e37ed
Rajkumar <rioraj1359@gmail.com>
2024-12-23 12:12:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// scroll section active link
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+
+};
+
+//
+// var btn = document.getElementById('btn')
+var loyout =document.getElementById('loyout')
+var para = document.getElementById('para')
+
+function add(){
+    loyout.style.display = 'block'
+    para.style.display = 'block'
+}
+function remove(){
+    loyout.style.display = 'none'
+    para.style.display = 'none'
+}
